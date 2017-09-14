884fd65c0c06c0ea0ce2e43692743fd7f3895795
Berkan Demirtas <demirtasberkan@gmail.com>
2017-09-14 13:24:32 +0300

    Update SettlementDaysCalculator.java

--- DailyTradeReportEngineJp/src/main/calculators/SettlementDaysCalculator.java
+++ DailyTradeReportEngineJp/src/main/calculators/SettlementDaysCalculator.java
@@ -9,11 +9,14 @@ import main.bean.days.WorkDaysDefult;
 
 public class SettlementDaysCalculator {
 //returns workdays according to currency of deal
+	
 	public static LocalDate calcDays(Deal dealItem) {
-		
+				
 			IWorkDays workday;
+
+//for SAR and AED returns Arabic working days, for others returns default working days		
 			if (dealItem.getCurrCode().getCurrencyCode().equals("SAR")
-					|| dealItem.getCurrCode().getCurrencyCode().equals("")) {
+					|| dealItem.getCurrCode().getCurrencyCode().equals("AED")) {
 				workday = WorkDaysArabic.getInstance();
 			}
 
